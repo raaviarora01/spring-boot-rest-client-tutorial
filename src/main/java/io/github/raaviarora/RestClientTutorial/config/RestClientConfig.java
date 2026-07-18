b6308a63bfa7a390f7f85856eed40f800ffb3578
raaviarora01 <aroraraavi01@gmail.com>
2026-07-18 11:41:51 +0530

    refactor: externalize API base URL configuration

--- src/main/java/io/github/raaviarora/RestClientTutorial/config/RestClientConfig.java
+++ src/main/java/io/github/raaviarora/RestClientTutorial/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package io.github.raaviarora.RestClientTutorial.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,10 +8,13 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+    @Value("${api.base-url}")
+    private String baseUrl;
+
     @Bean
     public RestClient getRestClient(){
         return RestClient.builder()
-                .baseUrl("https://jsonplaceholder.typicode.com")
+                .baseUrl(baseUrl)
                 .build();
     }
 }
