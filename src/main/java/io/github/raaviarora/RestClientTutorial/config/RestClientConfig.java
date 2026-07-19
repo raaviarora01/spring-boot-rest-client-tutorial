ca43bd679326a4069509c2647b84b86b1469ce00
raaviarora01 <aroraraavi01@gmail.com>
2026-07-19 13:10:49 +0530

    feat: configure default headers and bearer token authentication

--- src/main/java/io/github/raaviarora/RestClientTutorial/config/RestClientConfig.java
+++ src/main/java/io/github/raaviarora/RestClientTutorial/config/RestClientConfig.java
@@ -4,6 +4,8 @@ import io.github.raaviarora.RestClientTutorial.interceptor.LoggingInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
@@ -16,7 +18,8 @@ public class RestClientConfig {
     @Bean
     public RestClient getRestClient(@Value("${api.base-url}") String baseUrl,
                                     @Value("${api.connect-timeout}") int connectTimeout,
-                                    @Value("${api.read-timeout}") int readTimeout){
+                                    @Value("${api.read-timeout}") int readTimeout,
+                                    @Value("${api.bearer-token}") String bearerToken){
 
         HttpClient httpClient = HttpClient.newBuilder()
                 .connectTimeout(Duration.ofSeconds(connectTimeout))
@@ -27,6 +30,12 @@ public class RestClientConfig {
 
         return RestClient.builder()
                 .baseUrl(baseUrl)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeaders(headers -> {
+                    headers.setBearerAuth(bearerToken);
+                    headers.set("Client-Id", "restclient-tutorial");
+                })
                 .requestFactory(requestFactory)
                 .requestInterceptor(new LoggingInterceptor())
                 .build();
